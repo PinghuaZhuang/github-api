6e385daa7d299da7536b293c2419da8d1e04e7be
PinghuaZhuang <zphua2016@gmail.com>
2022-08-01 18:05:37 +0800

    feat: fly => axios. & add: delete.

--- src/request.ts
+++ src/request.ts
@@ -1,34 +1,38 @@
-import Fly from 'flyio/dist/npm/fly';
-import fly, { Fly as FlyImp, FlyResponse } from 'flyio';
-// import { READE_FILE } from './api';
+import axios, { AxiosRequestConfig, AxiosInstance, AxiosResponse } from 'axios'
+import merge from 'lodash/merge';
+import { READE_FILE, DELETE_FILE } from './api';
 
-export function createFly(engine?: any) {
-  const fly = new Fly(engine);
-
-  fly.config.headers = {
-    // 默认读取环境变量的 token
-    Authorization: GITHUB_TOKEN,
-    'Content-Type': 'application/json',
-  }
-
-  //设置超时
-  fly.config.timeout = 5 * 1_000;
-
-  //设置请求基地址
-  fly.config.baseURL = "https://api.github.com";
-
-  return fly;
+export function create(config?: AxiosRequestConfig) {
+  return axios.create(merge({
+    headers: {
+      Authorization: `Bearer ${GITHUB_TOKEN}`,
+      'Content-Type': 'application/json',
+      Accept: 'application/vnd.github+json',
+    },
+    timeout: 5 * 1_000,
+    baseURL: 'https://api.github.com',
+  }, config));
 };
 
 interface Config {
   token?: string;
-  fly?: FlyImp;
+  axios?: AxiosInstance;
   engine?: any;
   owner?: string;
   repo?: string;
 }
 
-type Options = FlyImp['config'] & Config;
+interface Message {
+  message: string;
+  sha: string;
+  branch: string;
+  committer?: {
+    name: string,
+    email: string,
+  }
+}
+
+type Options = AxiosRequestConfig & Config;
 
 function parseUrl(url: string, requset: Request) {
   return url.replace(/{owner}/g, requset.owner)
@@ -39,12 +43,12 @@ class Request {
   token!: string;
   owner!: string;
   repo!: string;
-  fly!: FlyImp;
+  axios!: AxiosInstance;
   ref = 'api';
 
   constructor(options?: Options) {
     this.config(options);
-    this.fly = options?.fly ?? createFly(options?.engine);
+    this.axios = options?.axios ?? create();
   }
 
   config(options?: Options) {
@@ -55,29 +59,52 @@ class Request {
     this.repo = options?.repo ?? (GITHUB_REPO as string);
   }
 
-  read(path: string) {
-    let error
-    if ((error = Request.validPath(path))) {
-      return Promise.reject(error);
-    }
-    return fly.get(
-      parseUrl(`{owner}/{repo}/api/${path.replace(/(.*?)(\.json)?$/, '$1.json')}`, this).replace(/{path}/g, path),
-      {
-        ref: this.ref,
-      },
-      {
+  read(path: string, params?: object, config?: Options) {
+    return axios.get(
+      parseUrl(
+        `{owner}/{repo}/api/${path.replace(/(.*?)(\.json)?$/, '$1.json')}`,
+        this
+      ).replace(/{path}/g, path),
+      merge({
+        params: merge({
+          ref: this.ref,
+        }, params),
         headers: {
           'Content-Type': 'text/plain',
         },
         baseURL: 'https://raw.githubusercontent.com',
-      },
-    ).then((response: FlyResponse<string>) => JSON.parse(response.data));
+      }, config),
+    ).then((response: AxiosResponse<any>) => response.data);
   }
 
-  static validPath(path: string) {
-    if (/\.json$/.test(path)) {
-      return new Error(`path must be json.`);
-    }
+  delete(path: string, message?: Message, params?: object) {
+    const suffixPath = path.replace(/(.*?)(\.json)?$/, '$1.json');
+    const mergeParams = merge({
+      ref: this.ref,
+    }, params);
+    return this.axios.get(
+      parseUrl(
+        READE_FILE,
+        this
+      ).replace(/{path}/g, suffixPath),
+      {
+        params: mergeParams,
+      },
+    ).then((response: AxiosResponse<{
+      sha: string;
+    }>) => {
+      return this.axios.delete(parseUrl(
+        DELETE_FILE,
+        this
+      ).replace(/{path}/g, suffixPath), {
+        data: merge({
+          message: `delete(GIT-API): ${suffixPath}`,
+          branch: this.ref,
+          sha: response.data.sha,
+          committer: { name: "GITHUB-API" },
+        }, message),
+      });
+    })
   }
 }
 
